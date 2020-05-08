c7b840ed6cbdf2496132be3d6e9b59a1ea0e3b1f
Admin <karolinaoparczyk@gmail.com>
2020-05-08 14:00:44 +0200

    Login through api

--- lib/pages/setup/sign_in.dart
+++ lib/pages/setup/sign_in.dart
@@ -1,36 +1,153 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:io';
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/dio.dart' as dio;
 import 'package:idom/pages/setup/accounts.dart';
+import 'package:path_provider/path_provider.dart';
 
+final storage = FlutterSecureStorage();
 
-class SignIn extends StatefulWidget{
+class SignIn extends StatefulWidget {
   @override
   _SignInState createState() => new _SignInState();
 }
 
-class _SignInState extends State<SignIn>{
-
+class _SignInState extends State<SignIn> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String _login, _password;
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final dio.Dio _dio = dio.Dio();
+  PersistCookieJar persistentCookies;
+  final String URL = "http://10.0.2.2:8000/";
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<Directory> get _localCoookieDirectory async {
+    final path = await _localPath;
+    final Directory dir = new Directory('$path/cookies');
+    await dir.create();
+    return dir;
+  }
+
+  Future<String> getCsrftoken() async {
+    try {
+      String csrfTokenValue;
+      final Directory dir = await _localCoookieDirectory;
+      final cookiePath = dir.path;
+      persistentCookies = new PersistCookieJar(dir: '$cookiePath');
+      persistentCookies
+          .deleteAll(); //clearing any existing cookies for a fresh start
+      _dio.interceptors.add(dio.CookieManager(
+              persistentCookies) //this sets up _dio to persist cookies throughout subsequent requests
+          );
+      _dio.options = new dio.BaseOptions(
+        baseUrl: URL,
+        contentType: ContentType.json,
+        responseType: dio.ResponseType.plain,
+        connectTimeout: 5000,
+        receiveTimeout: 100000,
+        headers: {
+          HttpHeaders.userAgentHeader: "dio",
+          "Connection": "keep-alive",
+        },
+      ); //BaseOptions will be persisted throughout subsequent requests made with _dio
+      _dio.interceptors
+          .add(dio.InterceptorsWrapper(onResponse: (dio.Response response) {
+        List<Cookie> cookies = persistentCookies.loadForRequest(Uri.parse(URL));
+        csrfTokenValue = cookies
+            .firstWhere((c) => c.name == 'csrftoken', orElse: () => null)
+            ?.value;
+        if (csrfTokenValue != null) {
+          _dio.options.headers['X-CSRF-TOKEN'] =
+              csrfTokenValue; //setting the csrftoken from the response in the headers
+        }
+        return response;
+      }));
+      await _dio.get("login/");
+      return csrfTokenValue;
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return null;
+    }
+  }
+
+  attemptToSignIn() async {
+    try {
+      final formState = _formKey.currentState;
+      if (formState.validate()) {
+        formState.save();
+        try {
+          final csrf = await getCsrftoken();
+          dio.FormData formData = new dio.FormData.from({
+            "username": _usernameController.value.text,
+            "password": _passwordController.value.text,
+            "csrfmiddlewaretoken": '$csrf'
+          });
+          dio.Options optionData = new dio.Options(
+            followRedirects: false,
+            validateStatus: (status) {
+              return status < 500;
+            },
+            contentType: ContentType.parse("application/x-www-form-urlencoded"),
+          );
+          dio.Response response =
+              await _dio.post("login/", data: formData, options: optionData);
+          if (response.statusCode == 200 &&
+              response.data
+                  .toString()
+                  .contains("Please enter a correct username and password")) {
+            displayDialog(context, "Błąd logowania",
+                "Błędne hasło lub konto z podanym loginem nie istnieje");
+          } else if (response.statusCode == 200 || response.statusCode == 302) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Accounts()));
+          }
+        } on dio.DioError catch (e) {
+          if (e.response != null) {
+            print(e.response.statusCode.toString() +
+                " " +
+                e.response.statusMessage);
+            print(e.response.data);
+            print(e.response.headers);
+            print(e.response.request);
+          } else {
+            print(e.request);
+            print(e.message);
+          }
+        } catch (error, stacktrace) {
+          print("Exception occured: $error stackTrace: $stacktrace");
+          return null;
+        }
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 
   Widget _buildLogin() {
     return TextFormField(
+        controller: _usernameController,
         decoration: InputDecoration(
-          labelText: 'Login',
-          labelStyle: TextStyle(color: Colors.black, fontSize: 18)
-          ),
+            labelText: 'Login',
+            labelStyle: TextStyle(color: Colors.black, fontSize: 18)),
         validator: (String value) {
           if (value.isEmpty) {
             return 'Podaj login';
           }
-        },
-        onSaved: (String value) {
-          _login = value;
         });
   }
 
   Widget _buildPassword() {
     return TextFormField(
+      controller: _passwordController,
       decoration: InputDecoration(
         labelText: 'Hasło',
         labelStyle: TextStyle(color: Colors.black, fontSize: 18),
@@ -40,13 +157,9 @@ class _SignInState extends State<SignIn>{
           return 'Podaj hasło';
         }
       },
-      onSaved: (String value) {
-        _password = value;
-      },
       obscureText: true,
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -73,7 +186,7 @@ class _SignInState extends State<SignIn>{
                           SizedBox(
                             width: 190,
                             child: RaisedButton(
-                                onPressed: signIn,
+                                onPressed: attemptToSignIn,
                                 child: Text(
                                   'Zaloguj się',
                                   style: TextStyle(
@@ -86,7 +199,7 @@ class _SignInState extends State<SignIn>{
                                 elevation: 10,
                                 shape: new RoundedRectangleBorder(
                                     borderRadius:
-                                    new BorderRadius.circular(30.0))),
+                                        new BorderRadius.circular(30.0))),
                           ),
                         ],
                       ),
@@ -98,18 +211,10 @@ class _SignInState extends State<SignIn>{
     );
   }
 
-  Future<void> signIn() async {
-    final formState = _formKey.currentState;
-    if(formState.validate()){
-      formState.save();
-      try{
-        //user = await database login
-        Navigator.push(context, MaterialPageRoute(builder: (context) => Accounts()));
-      }
-      catch(e){
-        print(e.toString());
-      }
-      //sign in to database
-    }
-  }
+  void displayDialog(BuildContext context, String title, String text) =>
+      showDialog(
+        context: context,
+        builder: (context) =>
+            AlertDialog(title: Text(title), content: Text(text)),
+      );
 }
