0586920abd2ffd029c56b08dcb1c9c40fdd58d12
Admin <karolinaoparczyk@gmail.com>
2020-06-01 00:12:32 +0200

    Tests: Delete sensor IDOM-133

--- test/widget/sensors_view_test.dart
+++ test/widget/sensors_view_test.dart
@@ -45,4 +45,126 @@ void main() {
     await tester.pumpAndSettle();
     expect(find.byType(ListTile).evaluate().length, 2);
   });
+
+
+  /// tests if deletes sensor after confirmation
+  testWidgets('sensors on list, confirms, deletes', (WidgetTester tester) async {
+    MockApi mockApi = MockApi();
+    List<Sensor> sensors = List();
+    sensors.add(Sensor(
+        id: 1,
+        name: "sensor1",
+        category: "temperature",
+        batteryLevel: null,
+        notifications: true,
+        isActive: false));
+    sensors.add(Sensor(
+        id: 2,
+        name: "sensor2",
+        category: "temperature",
+        batteryLevel: null,
+        notifications: true,
+        isActive: false));
+
+    Sensors page = Sensors(
+      currentLoggedInToken: "token",
+      currentLoggedInUsername: "username",
+      api: mockApi,
+      testSensors: sensors,
+    );
+
+    await tester.pumpWidget(makeTestableWidget(child: page));
+    await tester.pumpAndSettle();
+    expect(find.byType(ListTile).evaluate().length, 2);
+
+    await tester.tap(find.byType(FlatButton).first);
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byKey(Key('yesButton')));
+    await tester.pumpAndSettle();
+
+    verify(await mockApi.deactivateSensor(1, "token")).called(1);
+  });
+
+  /// tests if does not delete sensor when no confirmation
+  testWidgets('sensors on list, does not confirm, does not delete', (WidgetTester tester) async {
+    MockApi mockApi = MockApi();
+    List<Sensor> sensors = List();
+    sensors.add(Sensor(
+        id: 1,
+        name: "sensor1",
+        category: "temperature",
+        batteryLevel: null,
+        notifications: true,
+        isActive: false));
+    sensors.add(Sensor(
+        id: 2,
+        name: "sensor2",
+        category: "temperature",
+        batteryLevel: null,
+        notifications: true,
+        isActive: false));
+
+    Sensors page = Sensors(
+      currentLoggedInToken: "token",
+      currentLoggedInUsername: "username",
+      api: mockApi,
+      testSensors: sensors,
+    );
+
+    await tester.pumpWidget(makeTestableWidget(child: page));
+    await tester.pumpAndSettle();
+    expect(find.byType(ListTile).evaluate().length, 2);
+
+    await tester.tap(find.byType(FlatButton).first);
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byKey(Key('noButton')));
+    await tester.pumpAndSettle();
+
+    verifyNever(await mockApi.deactivateSensor(1, "token"));
+  });
+
+  /// tests eror message when api error
+  testWidgets('sensors on list, api error, message to user', (WidgetTester tester) async {
+    MockApi mockApi = MockApi();
+    when(mockApi.deactivateSensor(1, "token"))
+        .thenAnswer((_) async => Future.value(404));
+    List<Sensor> sensors = List();
+    sensors.add(Sensor(
+        id: 1,
+        name: "sensor1",
+        category: "temperature",
+        batteryLevel: null,
+        notifications: true,
+        isActive: false));
+    sensors.add(Sensor(
+        id: 2,
+        name: "sensor2",
+        category: "temperature",
+        batteryLevel: null,
+        notifications: true,
+        isActive: false));
+
+    Sensors page = Sensors(
+      currentLoggedInToken: "token",
+      currentLoggedInUsername: "username",
+      api: mockApi,
+      testSensors: sensors,
+    );
+
+    await tester.pumpWidget(makeTestableWidget(child: page));
+    await tester.pumpAndSettle();
+    expect(find.byType(ListTile).evaluate().length, 2);
+
+    await tester.tap(find.byType(FlatButton).first);
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byKey(Key('yesButton')));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byKey(Key('ok button')));
+
+    verify(await mockApi.deactivateSensor(1, "token")).called(1);
+  });
 }
