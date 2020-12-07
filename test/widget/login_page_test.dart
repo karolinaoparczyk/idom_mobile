60e9d6749cdfc2f8d35849cd6571870a7fedc82a
Admin <karolinaoparczyk@gmail.com>
2020-12-07 17:14:48 +0100

    IDOM-462

--- test/widget/login_page_test.dart
+++ test/widget/login_page_test.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:i18n_extension/i18n_widget.dart';
 import 'package:idom/utils/secure_storage.dart';
 import 'package:mockito/mockito.dart';
 
@@ -13,10 +15,27 @@ class MockApi extends Mock implements Api {}
 class MockSecureStorage extends Mock implements SecureStorage {}
 
 void main() {
-  Widget makeTestableWidget({Widget child}) {
+  Widget makePolishTestableWidget({Widget child}) {
     return MaterialApp(
       home: child,
     );
+  }
+
+  Widget makeEnglishTestableWidget({Widget child}) {
+    return MaterialApp(
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('en', "UK"),
+          Locale('pl', "PL"),
+        ],
+        localeListResolutionCallback: (locales, supportedLocales) {
+          return Locale('en', "UK");
+        },
+        home: I18n(child: child));
   }
 
   /// tests if signs in with empty email or password
@@ -28,10 +47,14 @@ void main() {
     SignIn page = SignIn(
         storage: mockSecureStorage, isFromSignUp: false, testApi: mockApi);
 
-    await tester.pumpWidget(makeTestableWidget(child: page));
+    await tester.pumpWidget(makePolishTestableWidget(child: page));
 
+    expect(find.text("Nazwa użytkownika"), findsOneWidget);
+    expect(find.text("Hasło"), findsOneWidget);
     await tester.tap(find.byKey(Key('Zaloguj')));
+    await tester.pumpAndSettle();
 
+    expect(find.text("Pole wymagane"), findsNWidgets(2));
     verifyNever(mockApi.signIn('', ''));
   });
 
@@ -39,17 +62,17 @@ void main() {
   testWidgets('email and password non-empty, success sign in',
       (WidgetTester tester) async {
     MockApi mockApi = MockApi();
-    var userApi = {
+    Map<String, dynamic> userApi = {
       "id": 1,
-      "username": "user1",
+      "username": "username",
       "email": "user@email.pl",
       "telephone": "+48765677655",
       "sms_notifications": true,
-      "app_notifications": true,
       "is_staff": false,
       "is_active": true,
     };
-    when(mockApi.getUser('user1'))
+    when(mockApi.getUser('username',
+            userToken: "\"wertyuiopasdfghjklzxcvbnmwertyuiopasdf\""))
         .thenAnswer((_) async => Future.value([jsonEncode(userApi), 200]));
     List<dynamic> res = [
       '"token": "wertyuiopasdfghjklzxcvbnmwertyuiopasdf"',
@@ -62,7 +85,7 @@ void main() {
     SignIn page = SignIn(
         storage: mockSecureStorage, isFromSignUp: false, testApi: mockApi);
 
-    await tester.pumpWidget(makeTestableWidget(child: page));
+    await tester.pumpWidget(makePolishTestableWidget(child: page));
 
     Finder usernameField = find.byKey(Key('username'));
     await tester.enterText(usernameField, 'username');
@@ -79,7 +102,6 @@ void main() {
   testWidgets('email and password non-empty, error sign in',
       (WidgetTester tester) async {
     MockApi mockApi = MockApi();
-    bool isSignedIn = false;
     List<dynamic> res = ['Wrong credentials', 400];
     when(mockApi.signIn('email@mail.com', 'password'))
         .thenAnswer((_) => Future.value(res));
@@ -88,17 +110,71 @@ void main() {
     SignIn page = SignIn(
         storage: mockSecureStorage, isFromSignUp: false, testApi: mockApi);
 
-    await tester.pumpWidget(makeTestableWidget(child: page));
+    await tester.pumpWidget(makePolishTestableWidget(child: page));
 
-    Finder emailField = find.byKey(Key('email'));
-    await tester.enterText(emailField, 'email@mail.com');
+    Finder usernameField = find.byKey(Key('username'));
+    await tester.enterText(usernameField, 'email@mail.com');
 
     Finder passwordField = find.byKey(Key('password'));
     await tester.enterText(passwordField, 'password');
 
-    await tester.tap(find.byKey(Key('Zaloguj się')));
+    await tester.tap(find.byKey(Key('Zaloguj')));
+    await tester.pumpAndSettle();
 
+    expect(
+        find.text(
+            "Błąd logowania. Błędne hasło lub konto z podanym loginem nie istnieje."),
+        findsOneWidget);
     verify(mockApi.signIn('email@mail.com', 'password')).called(1);
-    expect(isSignedIn, false);
+  });
+
+  /// tests if signs in with empty email or password, english
+  testWidgets('english email or password is empty, does not sign in',
+      (WidgetTester tester) async {
+    MockApi mockApi = MockApi();
+    MockSecureStorage mockSecureStorage = MockSecureStorage();
+
+    SignIn page = SignIn(
+        storage: mockSecureStorage, isFromSignUp: false, testApi: mockApi);
+
+    await tester.pumpWidget(makeEnglishTestableWidget(child: page));
+
+    expect(find.text("Username"), findsOneWidget);
+    expect(find.text("Password"), findsOneWidget);
+    await tester.tap(find.byKey(Key('Sign in')));
+    await tester.pumpAndSettle();
+
+    expect(find.text("Required field"), findsNWidgets(2));
+    verifyNever(mockApi.signIn('', ''));
+  });
+
+  /// tests if does not sign in with invalid email or password, english
+  testWidgets('english email and password non-empty, error sign in',
+      (WidgetTester tester) async {
+    MockApi mockApi = MockApi();
+    List<dynamic> res = ['Wrong credentials', 400];
+    when(mockApi.signIn('email@mail.com', 'password'))
+        .thenAnswer((_) => Future.value(res));
+    MockSecureStorage mockSecureStorage = MockSecureStorage();
+
+    SignIn page = SignIn(
+        storage: mockSecureStorage, isFromSignUp: false, testApi: mockApi);
+
+    await tester.pumpWidget(makeEnglishTestableWidget(child: page));
+
+    Finder usernameField = find.byKey(Key('username'));
+    await tester.enterText(usernameField, 'email@mail.com');
+
+    Finder passwordField = find.byKey(Key('password'));
+    await tester.enterText(passwordField, 'password');
+
+    await tester.tap(find.byKey(Key('Sign in')));
+    await tester.pumpAndSettle();
+
+    expect(
+        find.text(
+            "Login error. Wrong password or account with the given login does not exist."),
+        findsOneWidget);
+    verify(mockApi.signIn('email@mail.com', 'password')).called(1);
   });
 }
