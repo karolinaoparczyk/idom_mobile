3cf04b4c7988794d467d445601d1d3ec1eeec22d
karo <karolinaoparczyk@gmail.com>
2021-01-11 20:37:05 +0100

    remote control address fix

--- lib/remote_control.dart
+++ lib/remote_control.dart
@@ -12,7 +12,7 @@ class RemoteControl {
     }
 
     var res = await httpClient
-        .post('${driver.ipAddress}/receive?name=${driver.name}&data=$command}')
+        .post('${driver.ipAddress}:8000/receive?name=${driver.name}&data=$command}')
         .timeout(Duration(seconds: 5));
 
     return res.statusCode;
